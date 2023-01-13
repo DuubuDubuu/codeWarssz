c2be26f681f39be46888e829e40d2b69f716bfa5
Lance Montiague <105115967+DuubuDubuu@users.noreply.github.com>
2023-01-14 01:41:31 +0800

    Add files via upload

---
+++ codewars.js
@@ -0,0 +1,14 @@
+//Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+//[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+function grow(x) {
+  // Create an array
+
+  var output = 1;
+
+  for (let i = 0; i < x.length; i++) {
+    output *= x[i];
+  }
+  return output;
+}
